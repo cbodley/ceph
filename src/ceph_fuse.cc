chanshimudingxi/ceph-ci

monc: Remove finisher, use io_context
Of course now everyone has to feed an io_context into the MonClient.

Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

src/ceph_fuse.cc
@@ -16,7 +16,9 @@
 #include <sys/utsname.h>
 #include <iostream>
 #include <string>
+#include <optional>
 
+#include "common/asio_misc.h"
 #include "common/config.h"
 #include "common/errno.h"
 
@@ -43,6 +45,8 @@
 #include <fuse.h>
 
 #define dout_context g_ceph_context
+
+std::optional<ceph::io_context_pool> icp;
 
 static void fuse_usage()
 {
@@ -223,7 +227,8 @@ int main(int argc, const char **argv, const char *envp[]) {
     int tester_r = 0;
     void *tester_rp = nullptr;
 
-    MonClient *mc = new MonClient(g_ceph_context);
+    icp.emplace(g_ceph_context);
+    MonClient *mc = new MonClient(g_ceph_context, *icp);
     int r = mc->build_initial_monmap();
     if (r == -EINVAL) {
       cerr << "failed to generate initial mon list" << std::endl;
@@ -238,7 +243,7 @@ int main(int argc, const char **argv, const char *envp[]) {
     messenger->set_policy(entity_name_t::TYPE_MDS,
 			  Messenger::Policy::lossless_client(0));
 
-    client = new StandaloneClient(messenger, mc);
+    client = new StandaloneClient(messenger, mc, *icp);
     if (filer_flags) {
       client->set_filer_flags(filer_flags);
     }
@@ -324,6 +329,7 @@ int main(int argc, const char **argv, const char *envp[]) {
     free(newargv);
     delete mc;
     mc = nullptr;
+    icp->finish();
     //cout << "child done" << std::endl;
     return forker.signal_exit(r);
   }

src/ceph_syn.cc
@@ -18,6 +18,7 @@
 
 #include "common/config.h"
 
+#include "common/asio_misc.h"
 #include "client/SyntheticClient.h"
 #include "client/Client.h"
 
@@ -50,7 +51,8 @@ int main(int argc, const char **argv, char *envp[])
   pick_addresses(g_ceph_context, CEPH_PICK_ADDRESS_PUBLIC);
 
   // get monmap
-  MonClient mc(g_ceph_context);
+  ceph::io_context_pool  poolctx(g_ceph_context, 1);
+  MonClient mc(g_ceph_context, poolctx);
   if (mc.build_initial_monmap() < 0)
     return -1;
 
@@ -64,9 +66,9 @@ int main(int argc, const char **argv, char *envp[])
     messengers[i] = Messenger::create_client_messenger(g_ceph_context,
 						       "synclient");
     messengers[i]->bind(g_conf()->public_addr);
-    mclients[i] = new MonClient(g_ceph_context);
+    mclients[i] = new MonClient(g_ceph_context, poolctx);
     mclients[i]->build_initial_monmap();
-    auto client = new StandaloneClient(messengers[i], mclients[i]);
+    auto client = new StandaloneClient(messengers[i], mclients[i], poolctx);
     client->set_filer_flags(syn_filer_flags);
     SyntheticClient *syn = new SyntheticClient(client);
     clients.push_back(client);
@@ -97,6 +99,6 @@ int main(int argc, const char **argv, char *envp[])
     messengers[i]->wait();
     delete messengers[i];
   }
+  poolctx.finish();
   return 0;
 }
-

src/libcephfs.cc
@@ -20,6 +20,7 @@
 #include "auth/Crypto.h"
 #include "client/Client.h"
 #include "librados/RadosClient.h"
+#include "common/asio_misc.h"
 #include "common/Mutex.h"
 #include "common/ceph_argparse.h"
 #include "common/common_init.h"
@@ -37,6 +38,7 @@
 #define DEFAULT_UMASK 002
 
 static mode_t umask_cb(void *);
+std::optional<ceph::io_context_pool> icp;
 
 struct ceph_mount_info
 {
@@ -78,18 +80,19 @@ public:
   int init()
   {
     common_init_finish(cct);
+    icp.emplace(cct);
 
     int ret;
 
     {
-      MonClient mc_bootstrap(cct);
+      MonClient mc_bootstrap(cct, *icp);
       ret = mc_bootstrap.get_monmap_and_config();
       if (ret < 0)
 	return ret;
     }
 
     //monmap
-    monclient = new MonClient(cct);
+    monclient = new MonClient(cct, *icp);
     ret = -CEPHFS_ERROR_MON_MAP_BUILD; //defined in libcephfs.h;
     if (monclient->build_initial_monmap() < 0)
       goto fail;
@@ -99,7 +102,7 @@ public:
 
     //at last the client
     ret = -CEPHFS_ERROR_NEW_CLIENT; //defined in libcephfs.h;
-    client = new StandaloneClient(messenger, monclient);
+    client = new StandaloneClient(messenger, monclient, *icp);
     if (!client)
       goto fail;
 
@@ -190,6 +193,9 @@ public:
     if (client) {
       delete client;
       client = nullptr;
+    }
+    if (icp) {
+      icp->finish();
     }
   }
 

cbodley/ceph

src/ceph_fuse.cc
@@ -16,7 +16,9 @@
 #include <sys/utsname.h>
 #include <iostream>
 #include <string>
+#include <optional>
 
+#include "common/asio_misc.h"
 #include "common/config.h"
 #include "common/errno.h"
 
@@ -43,6 +45,8 @@
 #include <fuse.h>
 
 #define dout_context g_ceph_context
+
+std::optional<ceph::io_context_pool> icp;
 
 static void fuse_usage()
 {
@@ -223,7 +227,8 @@ int main(int argc, const char **argv, const char *envp[]) {
     int tester_r = 0;
     void *tester_rp = nullptr;
 
-    MonClient *mc = new MonClient(g_ceph_context);
+    icp.emplace(g_ceph_context);
+    MonClient *mc = new MonClient(g_ceph_context, *icp);
     int r = mc->build_initial_monmap();
     if (r == -EINVAL) {
       cerr << "failed to generate initial mon list" << std::endl;
@@ -238,7 +243,7 @@ int main(int argc, const char **argv, const char *envp[]) {
     messenger->set_policy(entity_name_t::TYPE_MDS,
 			  Messenger::Policy::lossless_client(0));
 
-    client = new StandaloneClient(messenger, mc);
+    client = new StandaloneClient(messenger, mc, *icp);
     if (filer_flags) {
       client->set_filer_flags(filer_flags);
     }
@@ -305,6 +310,7 @@ int main(int argc, const char **argv, const char *envp[]) {
     client->unmount();
     cfuse->finalize();
   out_shutdown:
+    icp->stop();
     client->shutdown();
   out_init_failed:
     unregister_async_signal_handler(SIGHUP, sighup_handler);

src/ceph_syn.cc
@@ -18,6 +18,7 @@
 
 #include "common/config.h"
 
+#include "common/asio_misc.h"
 #include "client/SyntheticClient.h"
 #include "client/Client.h"
 
@@ -50,7 +51,8 @@ int main(int argc, const char **argv, char *envp[])
   pick_addresses(g_ceph_context, CEPH_PICK_ADDRESS_PUBLIC);
 
   // get monmap
-  MonClient mc(g_ceph_context);
+  ceph::io_context_pool  poolctx(g_ceph_context, 1);
+  MonClient mc(g_ceph_context, poolctx);
   if (mc.build_initial_monmap() < 0)
     return -1;
 
@@ -64,9 +66,9 @@ int main(int argc, const char **argv, char *envp[])
     messengers[i] = Messenger::create_client_messenger(g_ceph_context,
 						       "synclient");
     messengers[i]->bind(g_conf()->public_addr);
-    mclients[i] = new MonClient(g_ceph_context);
+    mclients[i] = new MonClient(g_ceph_context, poolctx);
     mclients[i]->build_initial_monmap();
-    auto client = new StandaloneClient(messengers[i], mclients[i]);
+    auto client = new StandaloneClient(messengers[i], mclients[i], poolctx);
     client->set_filer_flags(syn_filer_flags);
     SyntheticClient *syn = new SyntheticClient(client);
     clients.push_back(client);
@@ -78,6 +80,8 @@ int main(int argc, const char **argv, char *envp[])
        p != synclients.end();
        ++p)
     (*p)->start_thread();
+
+  poolctx.stop();
 
   //cout << "waiting for client(s) to finish" << std::endl;
   while (!clients.empty()) {
@@ -99,4 +103,3 @@ int main(int argc, const char **argv, char *envp[])
   }
   return 0;
 }
-

src/libcephfs.cc
@@ -20,6 +20,7 @@
 #include "auth/Crypto.h"
 #include "client/Client.h"
 #include "librados/RadosClient.h"
+#include "common/asio_misc.h"
 #include "common/Mutex.h"
 #include "common/ceph_argparse.h"
 #include "common/common_init.h"
@@ -37,6 +38,7 @@
 #define DEFAULT_UMASK 002
 
 static mode_t umask_cb(void *);
+std::optional<ceph::io_context_pool> icp;
 
 struct ceph_mount_info
 {
@@ -84,8 +86,9 @@ public:
       cct->_log->start();
     }
 
+    icp.emplace(cct);
     {
-      MonClient mc_bootstrap(cct);
+      MonClient mc_bootstrap(cct, *icp);
       ret = mc_bootstrap.get_monmap_and_config();
       if (ret < 0)
 	return ret;
@@ -94,7 +97,7 @@ public:
     common_init_finish(cct);
 
     //monmap
-    monclient = new MonClient(cct);
+    monclient = new MonClient(cct, *icp);
     ret = -CEPHFS_ERROR_MON_MAP_BUILD; //defined in libcephfs.h;
     if (monclient->build_initial_monmap() < 0)
       goto fail;
@@ -104,7 +107,7 @@ public:
 
     //at last the client
     ret = -CEPHFS_ERROR_NEW_CLIENT; //defined in libcephfs.h;
-    client = new StandaloneClient(messenger, monclient);
+    client = new StandaloneClient(messenger, monclient, *icp);
     if (!client)
       goto fail;
 
@@ -202,6 +205,9 @@ public:
       messenger->wait();
       delete messenger;
       messenger = nullptr;
+    }
+    if (icp) {
+      icp->stop();
     }
     if (monclient) {
       delete monclient;