80b2cbd4c7737c7626d8cc9eb7dcc65d64e9b881
Casey Bodley <cbodley@redhat.com>
2020-11-16 17:15:16 -0500

    rgw: fix formatting in RGWListBucket_ObjStore_S3v2::get_params

--- src/rgw/rgw_rest_s3.cc
+++ src/rgw/rgw_rest_s3.cc
@@ -1491,19 +1491,19 @@ int RGWListBucket_ObjStore_S3::get_params(optional_yield y)
 
 int RGWListBucket_ObjStore_S3v2::get_params(optional_yield y)
 {
-int ret = get_common_params();
-if (ret < 0) {
-  return ret;
-}
-s->info.args.get_bool("fetch-owner", &fetchOwner, false);
-startAfter = s->info.args.get("start-after", &start_after_exist);
-continuation_token = s->info.args.get("continuation-token", &continuation_token_exist);
-if(!continuation_token_exist) {
-  marker = startAfter;
-} else {
-  marker = continuation_token;
-}
-return 0;
+  int ret = get_common_params();
+  if (ret < 0) {
+    return ret;
+  }
+  s->info.args.get_bool("fetch-owner", &fetchOwner, false);
+  startAfter = s->info.args.get("start-after", &start_after_exist);
+  continuation_token = s->info.args.get("continuation-token", &continuation_token_exist);
+  if(!continuation_token_exist) {
+    marker = startAfter;
+  } else {
+    marker = continuation_token;
+  }
+  return 0;
 }
 
 void RGWListBucket_ObjStore_S3::send_common_versioned_response()
