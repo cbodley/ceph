6a634eb3921a7697396b4051780222b737add935
Feng, Hualong <hualong.feng@intel.com>
2022-12-13 11:02:28 +0000

    Crypto: Changed thread mode to coroutine mode
    Pass yield of RGW to crypto plugin, so crypto can
    not block the thread.
    
    Signed-off-by: Feng, Hualong <hualong.feng@intel.com>

--- src/rgw/driver/rados/rgw_sal_rados.cc
+++ src/rgw/driver/rados/rgw_sal_rados.cc
@@ -3097,9 +3097,9 @@ int RadosAtomicWriter::prepare(optional_yield y)
   return processor.prepare(y);
 }
 
-int RadosAtomicWriter::process(bufferlist&& data, uint64_t offset)
+int RadosAtomicWriter::process(bufferlist&& data, uint64_t offset, optional_yield y)
 {
-  return processor.process(std::move(data), offset);
+  return processor.process(std::move(data), offset, y);
 }
 
 int RadosAtomicWriter::complete(size_t accounted_size, const std::string& etag,
@@ -3120,9 +3120,9 @@ int RadosAppendWriter::prepare(optional_yield y)
   return processor.prepare(y);
 }
 
-int RadosAppendWriter::process(bufferlist&& data, uint64_t offset)
+int RadosAppendWriter::process(bufferlist&& data, uint64_t offset, optional_yield y)
 {
-  return processor.process(std::move(data), offset);
+  return processor.process(std::move(data), offset, y);
 }
 
 int RadosAppendWriter::complete(size_t accounted_size, const std::string& etag,
@@ -3143,9 +3143,9 @@ int RadosMultipartWriter::prepare(optional_yield y)
   return processor.prepare(y);
 }
 
-int RadosMultipartWriter::process(bufferlist&& data, uint64_t offset)
+int RadosMultipartWriter::process(bufferlist&& data, uint64_t offset, optional_yield y)
 {
-  return processor.process(std::move(data), offset);
+  return processor.process(std::move(data), offset, y);
 }
 
 int RadosMultipartWriter::complete(size_t accounted_size, const std::string& etag,
