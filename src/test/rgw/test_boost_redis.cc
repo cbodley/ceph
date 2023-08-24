185d3aaf4e8469c7899bd6a3b3e15e4b073f1ce6
Samarah <samarah.uriarte@ibm.com>
2023-08-24 14:20:56 +0000

    test/redis: Add additional boost redis unit tests
    Signed-off-by: Samarah <samarah.uriarte@ibm.com>

--- src/test/rgw/test_boost_redis.cc
+++ src/test/rgw/test_boost_redis.cc
@@ -82,6 +82,44 @@ TEST_F(ConnectionFixture, HMSet) {
   worker->join();
 }
 
+TEST_F(ConnectionFixture, HGETALL) {
+  request req;
+  response<std::string,
+           std::map<std::string, std::string> > resp;
+
+  req.push("HMSET", "key", "data", "value");
+  req.push("HGETALL", "key");
+
+  conn->async_exec(req, resp, [&](auto ec, auto) {
+    ASSERT_EQ((bool)ec, 0);
+    EXPECT_EQ(std::get<0>(resp).value(), "OK");
+    EXPECT_EQ(std::get<1>(resp).value().size(), 1);
+
+    conn->cancel();
+  });
+
+  *work = std::nullopt;
+  worker->join();
+}
+
+TEST_F(ConnectionFixture, FLUSHALL) {
+  request req;
+  response<std::string, boost::redis::ignore_t> resp;
+
+  req.push("HMSET", "key", "data", "value");
+  req.push("FLUSHALL");
+
+  conn->async_exec(req, resp, [&](auto ec, auto) {
+    ASSERT_EQ((bool)ec, 0);
+    EXPECT_EQ(std::get<0>(resp).value(), "OK");
+
+    conn->cancel();
+  });
+
+  *work = std::nullopt;
+  worker->join();
+}
+
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
