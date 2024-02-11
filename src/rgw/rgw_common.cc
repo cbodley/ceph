eeb27260d07f1f94b6edab981e0406969cd41091
Casey Bodley <cbodley@redhat.com>
2024-02-11 12:29:44 -0500

    rgw: rename iam_user_policies to iam_identity_policies
    identity policies can come from iam groups and roles too
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_common.cc
+++ src/rgw/rgw_common.cc
@@ -1289,7 +1289,7 @@ bool verify_user_permission(const DoutPrefixProvider* dpp,
     // account users always require an Allow from identity-based policy
     mandatory_policy = true;
   }
-  return verify_user_permission(dpp, &ps, s->user_acl, s->iam_user_policies, s->session_policies, res, op, mandatory_policy);
+  return verify_user_permission(dpp, &ps, s->user_acl, s->iam_identity_policies, s->session_policies, res, op, mandatory_policy);
 }
 
 bool verify_user_permission_no_policy(const DoutPrefixProvider* dpp, 
@@ -1446,7 +1446,7 @@ bool verify_bucket_permission(const DoutPrefixProvider* dpp, req_state * const s
     return false;
   }
   return verify_bucket_permission(dpp, s, s->bucket->get_key(), s->user_acl, s->bucket_acl,
-                                  s->iam_policy, s->iam_user_policies, s->session_policies, op);
+                                  s->iam_policy, s->iam_identity_policies, s->session_policies, op);
 }
 
 // Authorize anyone permitted by the bucket policy, identity policies, session policies and the bucket owner
@@ -1459,7 +1459,7 @@ int verify_bucket_owner_or_policy(const DoutPrefixProvider* dpp,
   const auto arn = ARN(s->bucket->get_key());
   const auto effect = evaluate_iam_policies(
       dpp, s->env, *s->auth.identity, account_root, op, arn,
-      s->iam_policy, s->iam_user_policies, s->session_policies);
+      s->iam_policy, s->iam_identity_policies, s->session_policies);
   if (effect == Effect::Deny) {
     return -EACCES;
   }
@@ -1628,7 +1628,7 @@ bool verify_object_permission(const DoutPrefixProvider* dpp, req_state *s, uint6
                                   s->bucket_acl,
                                   s->object_acl,
                                   s->iam_policy,
-                                  s->iam_user_policies,
+                                  s->iam_identity_policies,
                                   s->session_policies,
                                   op);
 }
